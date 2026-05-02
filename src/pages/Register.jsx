b0cc5b7f1e7fccdc3f4342abff68c29a52f9aada
hello-cj <j.carl.restauro@gmail.com>
2026-05-03 00:08:18 +0800

    fix again but this time it will work

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -54,7 +54,7 @@ export default function Register() {
 
         <br /><br />
 
-        <button onClick={() => navigate("/")}>
+        <button onClick={() => navigate("/dashboard")}>
           Back to Login
         </button>
 
